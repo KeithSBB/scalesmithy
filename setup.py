ffd5406373c8f3306200ce872bbb881f1af1a011
KeithSBB <keith@santabayanian.com>
2024-12-30 10:38:51 -0800

    Added initial setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='scalesmithy',
+        version='0.1',
+        packages=[''],
+        url='https://github.com/KeithSBB/Scale_Smithy',
+        license='TBD',
+        author='KeithSBB',
+        author_email='keith@santabayanian.com',
+        description='Musical scales Tool'
+)
